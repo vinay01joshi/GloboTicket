01dccdbe6286a3b6d138162556c115673c8f5a81
Vinay Joshi <vinay01joshi@gmail.com>
2021-03-12 08:16:39 +0530

    Add New Test case for the CreateCategory

---
+++ GloboTicket.TicketManagement.Application.UnitTests/Categories/Commands/CreateCategoryTests.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistance;
+using GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory;
+using GloboTicket.TicketManagement.Application.Profiles;
+using GloboTicket.TicketManagement.Application.UnitTests.Mocks;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GloboTicket.TicketManagement.Application.UnitTests.Categories.Commands
+{
+    public class CreateCategoryTests
+    {
+        private readonly IMapper _mapper;
+        private readonly Mock<IAsyncRepository<Category>> _mockCategoryRespository;
+
+        public CreateCategoryTests()
+        {
+            _mockCategoryRespository = RepositoryMocks.GetCategoryRepository();
+
+            var configurationProvider = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<MappingProfile>();
+            });
+
+            _mapper = configurationProvider.CreateMapper();
+        }
+
+        [Fact]
+        public async Task Handle_ValidCategory_AddToCategoriesRepo()
+        {
+            var handler = new CreateCategoryCommandHandler(_mockCategoryRespository.Object, _mapper);
+
+            await handler.Handle(new CreateCategoryCommand() { Name = "Test" }, CancellationToken.None);
+
+            var allCategories = await _mockCategoryRespository.Object.ListAllAsync();
+
+            allCategories.Count.ShouldBe(5);
+        }
+    }
+}
